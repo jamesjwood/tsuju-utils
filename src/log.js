bcdb51fb2cd8a5c713d7624d8b5a79f3924e581e
James Wood <james.wood@datatransparency.com>
2013-02-05 14:51:06 +0000

    logging

--- src/log.js
+++ src/log.js
@@ -1,9 +1,21 @@
-module.exports = function(message){
-	logger.apply(this, [message]);
+module.exports = function (logger, name, functionToBeWrapped) {
+  "use strict";
+
+  console.dir();
+
+  var newLogger = {
+		log: function (message) {
+    logger.log(name + ": " + message);
+  }
 };
 
-var logger = "";
 
-module.exports.setLogger = function(log){
-	logger = log;
+  var wrappedFunction = function () {
+    newLogger.log('start');
+    var results = functionToBeWrapped.apply(newLogger, arguments);
+    newLogger.log('end');
+    return results;
+  };
+  return wrappedFunction;
 };
+
